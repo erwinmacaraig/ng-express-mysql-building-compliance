586f315ad8a68a6b7477f3eed4c852b10ae32357
Allan Delfin <adelfin@evacgroup.com.au>
2017-10-09 12:28:59 +0800

    added responsive login front-end

---
+++ src/ng/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core'; 
+import { NgForm } from '@angular/forms';
+declare var $: any;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  signInFormSubmit(f: NgForm){
+  	console.log(f);
+  }
+
+}
