1422398688f13d1ecd5f1d8a191469352abebb5e
Erwin Macaraig <erwin.macaraig@gmail.com>
2017-10-21 11:26:05 +0800

    new token table and alter for table users (occupation)

--- db-alter/token.sql
+++ db-alter/token.sql
@@ -1,7 +1,7 @@
 --
 -- Table structure for table `token`
 --
-
+DROP TABLE IF EXISTS token;
 CREATE TABLE `token` (
  `token_id` int(11) NOT NULL AUTO_INCREMENT,
  `token` varchar(100) NOT NULL,
@@ -10,7 +10,4 @@ CREATE TABLE `token` (
  `verified` tinyint(1) NOT NULL DEFAULT '0',
  `expiration_date` datetime NOT NULL,
  PRIMARY KEY (`token_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1
-
-ALTER TABLE `token`
-  ADD PRIMARY KEY (`token_id`);
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
