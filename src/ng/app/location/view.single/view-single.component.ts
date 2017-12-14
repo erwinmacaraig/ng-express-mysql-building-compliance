81b209ee6fab0bae9d4639fff3e7ef4459a8dcfe
Allan Delfin <adelfin@evacgroup.com.au>
2017-12-14 16:35:03 +0800

    added show existing sublocations from view sublocation page

--- src/ng/app/location/view.single/view-single.component.ts
+++ src/ng/app/location/view.single/view-single.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation, OnDestroy, OnChanges } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, OnDestroy, OnChanges, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { PlatformLocation } from '@angular/common';
 import { NgForm } from '@angular/forms';
@@ -10,7 +10,9 @@ import { AuthService } from '../../services/auth.service';
 import { LocationsService } from '../../services/locations';
 import { DonutService } from '../../services/donut';
 import { Countries } from '../../models/country.model';
-
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 declare var $: any;
 @Component({
@@ -43,6 +45,14 @@ export class ViewSingleLocation implements OnInit, OnDestroy, OnChanges {
     errorMessageModalSublocation = '';
     showLoaderModalSublocation = false;
     selectedLocationToArchive = {};
+
+    public locationsSublocations = [];
+
+    public sameSublocation = [];
+    public sameSublocationCopy = [];
+    public inpSublocationNameTwoWayData = "";
+    public selectedSubLocationFromModal = {};
+    @ViewChild('inputSublocation') public inputSublocation: ElementRef;
 
     constructor(
         private auth: AuthService,
@@ -97,6 +107,12 @@ export class ViewSingleLocation implements OnInit, OnDestroy, OnChanges {
 
         });
 
+        this.locationService.getSublocationsOfParent(this.locationID)
+            .subscribe((response) => {
+                this.locationsSublocations = response.data;
+                this.sameSublocationCopy = JSON.parse( JSON.stringify(this.locationsSublocations) );
+            });
+
 		// DONUT update
 		// this.donut.updateDonutChart('#specificChart', 30, true);
 	}
@@ -105,33 +121,50 @@ export class ViewSingleLocation implements OnInit, OnDestroy, OnChanges {
         if(form.valid){
             this.errorMessageModalSublocation = '';
             this.showLoaderModalSublocation = true;
-            this.locationService.createSubLocation({
-                name : form.controls.name.value,
-                parent_id : this.locationID
-            }).subscribe(
-                (response) => {
-                    this.showLoaderModalSublocation = false;
-                    this.errorMessageModalSublocation = '';
-
-                    if ('sublocations' in this.locationData == false){
-                        this.locationData['sublocations'] = [];
-                    }
-
-                    response.data['children'] = [];
-
-                    response.data['location_id'] = this.encryptDecrypt.encrypt(response.data['location_id']).toString();
-                    this.locationData['sublocations'].push(response.data);
-                    $('#modalAddSublocation').modal('close');
-
-                },
-                (msg) => {
-                    this.showLoaderModalSublocation = false;
-                    this.errorMessageModalSublocation = msg.error;
-                    setTimeout(() => {
+            if(Object.keys(this.selectedSubLocationFromModal).length > 0){
+                this.locationService.assignSublocations([this.selectedSubLocationFromModal['location_id']])
+                .subscribe((response) => {
+                    this.loc_sub = this.locationService.getById(this.locationID, (response) => {
+                        this.locationData.sublocations = response.sublocations;
+                        for (let i = 0; i < this.locationData['sublocations'].length; i++) {
+                            this.locationData['sublocations'][i]['location_id']
+                            = this.encryptDecrypt.encrypt(this.locationData['sublocations'][i].location_id).toString();
+                        }
+                        this.inpSublocationNameTwoWayData = '';
+                        this.showLoaderModalSublocation = false;
                         this.errorMessageModalSublocation = '';
-                    }, 2000);
-                }
-            );
+                        $('#modalAddSublocation').modal('close');
+                    });
+                });
+            }else{
+                this.locationService.createSubLocation({
+                    name : form.controls.name.value,
+                    parent_id : this.locationID
+                }).subscribe(
+                    (response) => {
+                        this.showLoaderModalSublocation = false;
+                        this.errorMessageModalSublocation = '';
+
+                        if ('sublocations' in this.locationData == false){
+                            this.locationData['sublocations'] = [];
+                        }
+
+                        response.data['children'] = [];
+
+                        response.data['location_id'] = this.encryptDecrypt.encrypt(response.data['location_id']).toString();
+                        this.locationData['sublocations'].push(response.data);
+                        $('#modalAddSublocation').modal('close');
+                    },
+                    (msg) => {
+                        this.showLoaderModalSublocation = false;
+                        this.errorMessageModalSublocation = msg.error;
+                        setTimeout(() => {
+                            this.errorMessageModalSublocation = '';
+                        }, 2000);
+                    }
+                );
+            }
+
         }else{
             form.controls.name.markAsDirty();
         }
@@ -182,12 +215,47 @@ export class ViewSingleLocation implements OnInit, OnDestroy, OnChanges {
 		return name;
 	}
 
+    onKeyUpTypeSublocation(value){
+        let trimmed = value.trim(),
+        trimmedLow = trimmed.toLowerCase(),
+        results = [];
+        if(trimmed.length == 0){
+            this.sameSublocation = [];
+        }else{
+            let searchChild = (children) => {
+              for(let i in children){
+                let name = children[i]['name'],
+                  low = name.toLowerCase();
+
+                if(low.indexOf(trimmedLow) > -1){
+                  results.push( JSON.parse(JSON.stringify(children[i])) );
+                }
+              }
+            };
+
+            searchChild(this.sameSublocationCopy);
+            this.sameSublocation = results;
+        }
+    }
+
+    selectAddNewSubResult(sub, selectElement){
+        $('.select-sub.red-text').removeClass('red-text').addClass('blue-text').html('select');
+        if($(selectElement).hasClass('blue-text')){
+          $(selectElement).removeClass('blue-text').addClass('red-text').html('selected');
+        }else{
+          $(selectElement).removeClass('red-text').addClass('blue-text').html('select');
+        }
+        $(this.inputSublocation.nativeElement).trigger('focusin');
+        this.inpSublocationNameTwoWayData = sub.name;
+        this.selectedSubLocationFromModal = sub;
+    }
+
 	ngOnDestroy() {
-    /*
-    this.sub.unsubsribe();
-    this.loc_sub.unsubsribe();
-    */
-}
+        /*
+        this.sub.unsubsribe();
+        this.loc_sub.unsubsribe();
+        */
+    }
 
 
 
