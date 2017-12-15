d607afaa96e01bff567e9ef6b9cb5eeaaf959ffd
Allan Delfin <adelfin@evacgroup.com.au>
2017-12-15 10:57:01 +0800

    added teams module add feature

--- src/ng/app/teams/add.wardens/add.wardens.ts
+++ src/ng/app/teams/add.wardens/add.wardens.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, OnDestroy, AfterViewInit } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { PlatformLocation } from '@angular/common';
 import { NgForm } from '@angular/forms';
@@ -12,11 +12,43 @@ declare var $: any;
 })
 export class TeamsAddWardenComponent implements OnInit, OnDestroy {
 
+	public addedUsers = [];
+	public userProperty = {
+		first_name : '',
+		last_name : '',
+		email_or_username : '',
+		account_role_id : 0,
+		eco_role_id : 0,
+		location_name : 'Select Location',
+		location_id : 0,
+		mobile_number : ''
+	};
+
 	constructor(){
 
 	}
 
 	ngOnInit(){}
+
+	ngAfterViewInit(){
+		$('.modal').modal({
+			dismissible: false
+		});
+	}
+
+	showModalCSV(){
+		$('#modaCsvUpload').modal('open');
+	}
+
+	showModalInvite(){
+		$('#modalInvite').modal('open');
+	}
+
+	addMoreRow(){
+		//a copy
+		let prop = JSON.parse(JSON.stringify(this.userProperty));
+		this.addedUsers.push( prop );
+	}
 
 	ngOnDestroy(){}
 }
