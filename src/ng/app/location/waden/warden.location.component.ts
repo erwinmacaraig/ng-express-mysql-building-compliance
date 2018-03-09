de865b3534b9f0c8f8a78cf877e5b7041338e97a
Allan Delfin <adelfin@evacgroup.com.au>
2018-03-09 15:32:45 +0800

    location redirection warden account and added some info in page. also added benchmarking selection in locations list

--- src/ng/app/location/waden/warden.location.component.ts
+++ src/ng/app/location/waden/warden.location.component.ts
@@ -21,6 +21,12 @@ declare var $: any;
 export class WardenLocationComponent implements OnInit, OnDestroy {
 
 	userData: Object;
+	userInitials = "";
+	roleName = "";
+
+	routeSubs;
+	routeParamsSubsc;
+	routeParams = {};
 
 	constructor(
 		private auth: AuthService,
@@ -32,11 +38,47 @@ export class WardenLocationComponent implements OnInit, OnDestroy {
 		private userService : UserService
 		){
 		this.userData = this.auth.getUserData();
+
+		this.userInitials = this.getInitials( this.userData['name'] );
+
+		for(let i in this.userData['roles']){
+			let role = this.userData['roles'][i];
+			if('role_name' in role){
+				this.roleName = role.role_name;
+			}
+		}
+
+		let trp = false,
+			frp = false;
+		for(let i in this.userData['roles']){
+			if(this.userData['roles'][i]['role_id'] == 1){
+				frp = true;
+			}
+			if(this.userData['roles'][i]['role_id'] == 2){
+				trp = true;
+			}
+		}
+
+		if(trp){
+			this.roleName = 'Tenant Responsible Personnel';
+		}
+
+		if(frp){
+			this.roleName = 'Facility Responsible Personnel';
+		}
+
+		this.routeSubs = this.router.events.subscribe((event) => {
+			console.log(event);
+		});
+
+		this.routeParamsSubsc = this.activatedRoute.params.subscribe((params) => {
+			this.routeParams = params;
+		});
 	}
 
 	ngOnInit(){
 		$('select').material_select();
-
+		console.log(this.routeParams);
 	}
 
 	ngAfterViewInit(){
@@ -45,10 +87,18 @@ export class WardenLocationComponent implements OnInit, OnDestroy {
 		$('.workspace.container').css('padding', '0% 1%');
 	}
 
-
+	getInitials(fullName){
+		if(fullName){
+			let initials = fullName.match(/\b\w/g) || [];
+			initials = ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
+			return initials;
+		}
+		return 'AA';
+	}
 
 	ngOnDestroy(){
 		$('.workspace.container').css('padding', '1% 2%');
+		this.routeSubs.unsubscribe();
 	}
 
 }
