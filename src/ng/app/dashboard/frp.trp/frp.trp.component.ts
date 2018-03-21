4244c7d4933e94a964a36524bd542b303975198f
Allan Delfin <adelfin@evacgroup.com.au>
2018-03-21 14:32:48 +0800

    FRP TRP Dashboard links

--- src/ng/app/dashboard/frp.trp/frp.trp.component.ts
+++ src/ng/app/dashboard/frp.trp/frp.trp.component.ts
@@ -9,6 +9,7 @@ import { UserService } from '../../services/users';
 import { LocationsService } from '../../services/locations';
 import { DashboardPreloaderService } from '../../services/dashboard.preloader';
 import { CourseService } from '../../services/course';
+import { EncryptDecryptService } from '../../services/encrypt.decrypt';
 import { Router, NavigationEnd  } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
@@ -22,7 +23,7 @@ declare var $: any;
 	selector: 'app-frptrp-dashboard',
 	templateUrl: './frp.trp.component.html',
 	styleUrls: ['./frp.trp.component.css'],
-	providers: [AccountsDataProviderService, AuthService, LocationsService, DashboardPreloaderService, UserService, DonutService, CourseService]
+	providers: [AccountsDataProviderService, AuthService, LocationsService, DashboardPreloaderService, UserService, DonutService, CourseService, EncryptDecryptService]
 })
 
 export class FrpTrpDashboardComponent implements OnInit, AfterViewInit, OnDestroy {
@@ -30,17 +31,31 @@ export class FrpTrpDashboardComponent implements OnInit, AfterViewInit, OnDestro
 	userData = {};
 
 	courses = [];
+	locations = [];
 
 	constructor(
 		private authService : AuthService,
 		private donut : DonutService,
-		private courseService : CourseService
+		private courseService : CourseService,
+		private dashboardService : DashboardPreloaderService,
+		private locationService : LocationsService,
+		private encryptDecrypt : EncryptDecryptService
 		){
 
 		this.userData = this.authService.getUserData();
 
 		this.courseService.myCourses(this.userData['userId'], (response) => {
 			this.courses = response.data;
+		});
+
+		this.locationService.getParentLocationsForListing(this.userData['accountId'], (res) => {
+			this.locations = res.locations;
+			if (this.locations.length > 0) {
+    			for (let i = 0; i < this.locations.length; i++) {
+    				this.locations[i]['location_id'] = this.encryptDecrypt.encrypt(this.locations[i].location_id);
+    			}
+    		}
+			this.dashboardService.hide();
 		});
 
 	}
@@ -50,6 +65,8 @@ export class FrpTrpDashboardComponent implements OnInit, AfterViewInit, OnDestro
 	}
 
 	ngAfterViewInit(){
+		this.dashboardService.show();
+
 		$('.workspace.container').css('padding', '2% 5%');
 		$('select').material_select();
 
