3f8594e3b2121b6f02de69d4a9c705905ddb8271
Ubuntu <ubuntu@ip-10-0-0-113.ap-southeast-2.compute.internal>
2018-03-26 07:12:16 +0000

    handled async call

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -26,9 +26,10 @@ export class PaymentRoute extends BaseRoute {
 
 
   public static async create(router: Router) {
-    try{
-      const gateway = new Gateway();
-      const config = await gateway.getActiveConfig();
+   let config;
+    const gateway = new Gateway(); 
+   try{
+      config = await gateway.getActiveConfig();
       if (config['gateway_code'] === 'paypal') {
         const paypal_config = {
           'port': 5000,
