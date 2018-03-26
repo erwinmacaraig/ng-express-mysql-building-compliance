e665323e4f3264d3455b518ab4c4ef918247917f
Erwin Malabanan Macaraig <Erwin.Macaraig@gbs.local>
2018-03-26 18:34:19 +0800

    update utils sync

--- src/models/util.sync.ts
+++ src/models/util.sync.ts
@@ -21,15 +21,22 @@ export class UtilsSync {
   public async getRootParent(records: Array<object>) {
     let tempParentId = 0;
     for (let i = 0; i < records.length; i++) {
-      let loc = new Location(records[i]['parent_id']);
-      await loc.load();
-      tempParentId = +loc.get('parent_id');
-      while (tempParentId !== -1) {
-        loc = null;
-        loc = new Location(tempParentId);
+      let loc;
+      if (records[i]['parent_id'] !== -1) {
+        loc = new Location(records[i]['parent_id']);
         await loc.load();
         tempParentId = +loc.get('parent_id');
+        while (tempParentId !== -1) {
+          loc = null;
+          loc = new Location(tempParentId);
+          await loc.load();
+          tempParentId = +loc.get('parent_id');
+        }
+      } else {
+        loc = new Location(records[i]['location_id']);
+        await loc.load();
       }
+
       if (loc.get('name') && loc.get('name').toString().length > 0) {
         records[i]['root_parent_name'] = loc.get('name').toString();
       } else if (loc.get('formatted_address')) {
