a325053c3bec20c887db7908c45e6ac3aab9a44b
Erwin Malabanan Macaraig <Erwin.Macaraig@gbs.local>
2018-04-10 07:07:21 +0800

    dashboard user update

--- src/ng/app/dashboard/user/user.component.ts
+++ src/ng/app/dashboard/user/user.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../../services/users';
 import { Component, OnInit, AfterViewInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { PlatformLocation } from '@angular/common';
@@ -5,7 +6,6 @@ import { ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { AccountsDataProviderService } from '../../services/accounts';
-import { UserService } from '../../services/users';
 import { LocationsService } from '../../services/locations';
 import { DashboardPreloaderService } from '../../services/dashboard.preloader';
 import { Router, NavigationEnd  } from '@angular/router';
@@ -21,23 +21,36 @@ declare var $: any;
 	selector: 'app-user-dashboard',
 	templateUrl: './user.component.html',
 	styleUrls: ['./user.component.css'],
-	providers: [AccountsDataProviderService, AuthService, LocationsService, DashboardPreloaderService, UserService, DonutService]
+	providers: [AccountsDataProviderService, AuthService, LocationsService, DashboardPreloaderService, DonutService]
 })
 
 export class UserDashboardComponent implements OnInit, AfterViewInit, OnDestroy {
 
 	userData = {};
+  training_percentage = 0;
+  assignedCourses = [];
+  trainings = [];
 
 	constructor(
-		private authService : AuthService,
-		private donut : DonutService
+		private authService: AuthService,
+    private donut: DonutService,
+    private userService: UserService
 		){
 
 		this.userData = this.authService.getUserData();
 
 	}
 
-	ngOnInit(){
+	ngOnInit() {
+    this.userService.getEmUserDashboardInfo().subscribe((response) => {
+      console.log(response);
+      this.training_percentage = parseInt(response['percentage_training'], 10);
+      this.assignedCourses = response['courses'];
+      this.trainings = response['trainings'];
+      this.donut.updateDonutChart('#specificChart', this.training_percentage, true);
+    }, (e) => {
+      console.log(e);
+    });
 
 	}
 
@@ -46,8 +59,8 @@ export class UserDashboardComponent implements OnInit, AfterViewInit, OnDestroy 
 		$('select').material_select();
 
 		// DONUT update
-        // Donut Service
-		// this.donut.updateDonutChart('#specificChart', 30, true);
+    // Donut Service
+
 	}
 
 	ngOnDestroy(){
