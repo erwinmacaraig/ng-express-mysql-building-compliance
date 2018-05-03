ebcda7ba8a6a6c16ff2f09aab62e273e12e3d6e0
Erwin Malabanan Macaraig <Erwin.Macaraig@gbs.local>
2018-05-03 19:24:02 +0800

    initial work on adding users

--- src/ng/app/app-routing.module.ts
+++ src/ng/app/app-routing.module.ts
@@ -93,6 +93,7 @@ import { AdminComponent } from './admin/admin.component';
 import { ListAccountsComponent } from './admin/list-accounts/list-accounts.component';
 import { AccountInfoComponent } from './admin/account-info/account-info.component';
 import { AccountUsersListComponent } from './admin/account-users/account-users.component';
+import { AddAccountUserComponent } from './admin/add-user/add-user.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent},
@@ -205,7 +206,8 @@ const appRoutes: Routes = [
     path: 'admin', canActivate: [AuthGuard], component: AdminComponent,
     children: [
       { path: 'accounts', component: ListAccountsComponent },
-      { path: 'users-in-accounts/:accntId', component: AccountUsersListComponent  }
+      { path: 'users-in-accounts/:accntId', component: AccountUsersListComponent },
+      { path: 'add-account-user/:accntId', component: AddAccountUserComponent }
 
     ]
   }
