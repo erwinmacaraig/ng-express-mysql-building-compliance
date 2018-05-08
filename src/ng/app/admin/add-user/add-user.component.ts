bdcbdd00f848adef0ae3993f19cab2e7144afdc6
Erwin Malabanan Macaraig <Erwin.Macaraig@gbs.local>
2018-05-08 15:33:00 +0800

    initial work on reactive form for adding users

--- src/ng/app/admin/add-user/add-user.component.ts
+++ src/ng/app/admin/add-user/add-user.component.ts
@@ -5,7 +5,7 @@ import { NgForm } from '@angular/forms';
 import { Router, NavigationStart, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { ViewChild, ElementRef } from '@angular/core';
 import { Subscription } from 'rxjs/Rx';
-
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 import { AdminService } from './../../services/admin.service';
 declare var $: any;
 
@@ -17,16 +17,93 @@ declare var $: any;
 })
 export class AddAccountUserComponent  implements OnInit {
   accountId = 0;
-  constructor(private route: ActivatedRoute) {}
+  userForm: FormGroup;
+  users;
+
+  buildings = [];
+  levels = [];
+  roles = [
+    {
+      role_id: 1,
+      role_name: 'Tenant Responsible Person'
+    },
+    {
+      role_id: 8,
+      role_name: 'General Occupant',
+    },
+    {
+      role_id: 9,
+      role_name: 'Warden',
+    },
+    {
+      role_id: 10,
+      role_name: 'Floor / Area Warden',
+    },
+    {
+      role_id: 11,
+      role_name: 'Chief Warden',
+    },
+    {
+      role_id: 12,
+      role_name: 'Fire Safety Advisor',
+    },
+    {
+      role_id: 13,
+      role_name: 'Emergency Planning Committee Member',
+    },
+    {
+      role_id: 14,
+      role_name: 'Fire Aid Officer',
+    },
+    {
+      role_id: 15,
+      role_name: 'Deputy Chief Warden',
+    },
+    {
+      role_id: 16,
+      role_name: 'Building Warden',
+    },
+    {
+      role_id: 18,
+      role_name: 'Deputy Building Warden',
+    },
+
+  ];
+  constructor(private formBuilder: FormBuilder, private adminService: AdminService, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.params.subscribe((parameters) => {
       this.accountId = parameters['accntId'];
+      this.userForm = this.formBuilder.group({
+        users: this.formBuilder.array([this.createFormItem()])
+      });
+
+
+      this.adminService.getAllLocationsOnAccount(this.accountId).subscribe((response) => {
+        this.buildings = response['data']['buildings'];
+        this.levels = response['data']['levels'];
+      });
 
       // get all locations tagged to this account
       // buildings
       // levels
     });
+  }
+  createFormItem(): FormGroup {
+    return this.formBuilder.group({
+      first_name: '',
+      last_name: '',
+      password: '',
+      email: '',
+      role: 0,
+      location: 0,
+      contact: ''
+    });
+  }
+
+  addUserFormItem(): void {
+    this.users = this.userForm.get('users') as FormArray;
+    this.users.push(this.createFormItem());
   }
 }
 
