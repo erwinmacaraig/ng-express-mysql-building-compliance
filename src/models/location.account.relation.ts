db7506746ba256a452c3d4728bf013a80596b054
Erwin Malabanan Macaraig <Erwin.Macaraig@gbs.local>
2018-05-21 12:21:16 +0800

    Fixed location listing error on the number of tenants

--- src/models/location.account.relation.ts
+++ src/models/location.account.relation.ts
@@ -57,17 +57,18 @@ export class LocationAccountRelation extends BaseClass {
         });
     }
 
-    public getByAccountIdAndLocationId(accountId, locationId) {
+    public getByAccountIdAndLocationId(accountId, locationId): Promise<Array<object>> {
         return new Promise((resolve, reject) => {
-            const sql_load = 'SELECT * FROM location_account_relation WHERE account_id = ? AND location_id = ? ORDER BY location_account_relation_id DESC';
+            const sql_load = `SELECT * FROM location_account_relation WHERE account_id = ?
+            AND location_id = ? ORDER BY location_account_relation_id DESC`;
             const param = [accountId, locationId];
             const connection = db.createConnection(dbconfig);
             connection.query(sql_load, param, (error, results, fields) => {
               if (error) {
-                return console.log(error);
+                console.log('location.account.relation.getByAccountIdAndLocationId', error);
+                throw Error('Internal Server Error');
               }
-              this.dbData = results;
-              resolve(this.dbData);
+              resolve(results);
             });
             connection.end();
         });
