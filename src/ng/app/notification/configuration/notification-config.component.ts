b9170fcb2d69f5f953ff4a91c37a6140fff7cc63
Erwin Malabanan Macaraig <Erwin.Macaraig@gbs.local>
2018-08-02 09:12:50 +0800

    update for notification

--- src/ng/app/notification/configuration/notification-config.component.ts
+++ src/ng/app/notification/configuration/notification-config.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
+
 
 declare var $: any;
 @Component({
@@ -8,9 +11,28 @@ declare var $: any;
 })
 export class NotificationConfigurationComponent implements OnInit, AfterViewInit, OnDestroy {
 
+  public notConfigFormGrp: FormGroup;
+  public buildingId = 0;
+  public searchBldgField: FormControl;
+  public trp_user: FormControl;
+  public all_users: FormControl;
+  public messageField: FormControl;
+  public frequency_field: FormControl;
+  public eco_user: FormControl;
+  private sub: Subscription;
+  private defaultMessage = 'Test Message';
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.notConfigFormGrp = new FormGroup({
+      all_users: new FormControl(false, null),
+      eco_user: new FormControl(false, null),
+      frequency_field: new FormControl(null, Validators.required),
+      trp_user: new FormControl(false, null),
+      messageField: new FormControl(this.defaultMessage, Validators.required)
+    });
+    this.searchBldgField = new FormControl();
+  }
 
   ngAfterViewInit() {}
 
