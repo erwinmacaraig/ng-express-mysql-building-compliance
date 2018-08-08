d4f235edb58e8e99b7ea44399b371b04097ad325
Allan Delfin <adelfin@evacgroup.com.au>
2018-08-08 12:07:45 +0800

    made add users location available to all roles

--- src/ng/app/teams/add-mobility-impaired/add.mobility.impaired.component.ts
+++ src/ng/app/teams/add-mobility-impaired/add.mobility.impaired.component.ts
@@ -190,10 +190,23 @@ export class AddMobilityImpairedComponent implements OnInit, OnDestroy {
     filterLocationsToDisplayByUserRole(user, data){
         let resp = [],
             copy = JSON.parse(JSON.stringify(data));
-        if(user.account_role_id == 1 || user.account_role_id == 11 || user.account_role_id == 15 || user.account_role_id == 16 || user.account_role_id == 18){
+        /*if(user.account_role_id == 1 || user.account_role_id == 11 || user.account_role_id == 15 || user.account_role_id == 16 || user.account_role_id == 18){
             resp = JSON.parse( JSON.stringify( this.buildings ) );
         }else{
             resp = JSON.parse( JSON.stringify( this.levels ) );
+        }*/
+
+        for(let loc of this.buildings){
+            loc['sublocations'] = [];
+            for(let level of this.levels){
+                if(loc.location_id == level.parent_location_id){
+                    for(let sub of level.sublocations){
+                        sub['location_id'] = sub.id;
+                    }
+                    loc['sublocations'] = level.sublocations;
+                }
+            }
+            resp.push(loc);
         }
 
         this.locationsCopy = JSON.parse( JSON.stringify( resp ) );
@@ -212,53 +225,87 @@ export class AddMobilityImpairedComponent implements OnInit, OnDestroy {
         });
 
         let maxDisplay = 25,
-            count = 1;
-            if (parseInt(this.selectedUser['account_role_id'], 10) === 1 ||
+        count = 1;
+        
+        /*if (parseInt(this.selectedUser['account_role_id'], 10) === 1 ||
             parseInt(this.selectedUser['account_role_id'], 10) === 11 ||
             parseInt(this.selectedUser['account_role_id'], 10) === 15 ||
             parseInt(this.selectedUser['account_role_id'], 10) === 16 ||
             parseInt(this.selectedUser['account_role_id'], 10) === 18
-           ) {
-          for (let loc of this.locations) {
-            if (count <= maxDisplay) {
-                let $li = $(`
-                    <li class="list-division" id="${loc.location_id}">
+            ) {
+            for (let loc of this.locations) {
+                if (count <= maxDisplay) {
+                    let $li = $(`
+                        <li class="list-division" id="${loc.location_id}">
                         <div class="name-radio-plus">
-                            <div class="input">
-                                <input required type="radio" name="selectLocation" value="${loc.location_id}" id="check-${loc.location_id}">
-                                <label for="check-${loc.location_id}">${loc.name}</label>
-                            </div>
+                        <div class="input">
+                        <input required type="radio" name="selectLocation" value="${loc.location_id}" id="check-${loc.location_id}">
+                        <label for="check-${loc.location_id}">${loc.name}</label>
                         </div>
-                    </li>`);
+                        </div>
+                        </li>`);
+
+                    ulModal.append($li);
+                    count++;
+                }
+            }
+        } else {
+            for (const loc of this.locations) {
+                if (count <= maxDisplay) {
+                    const $lh = $(`<lh><h6>${loc['parent_location_name']}</h6></lh>`);
+                    ulModal.append($lh);
+                    if ('sublocations' in loc) {
+                        for (const subloc of loc.sublocations) {
+                            const $li = $(`
+                                <li class="list-division" id="${subloc.id}">
+                                <div class="name-radio-plus">
+                                <div class="input">
+                                <input required type="radio"
+                                name="selectLocation"
+                                value="${subloc.id}" id="check-${subloc.id}">
+                                <label for="check-${subloc.id}">${subloc.name}</label>
+                                </div>
+                                </div>
+                                </li>`);
+                            ulModal.append($li);
+                        }
+                    }
+                    count++;
+                }
+            }
+        }*/
+
+        for (const loc of this.locations) {
+            if (count <= maxDisplay) {
+                let ul = ``;
+                if(loc.sublocations.length > 0){
+                    ul += '<ul style="padding-left: 20px; max-height: 153px; overflow: auto;">';
+                        for(let sub of loc.sublocations){
+                            ul += `<li class="list-division" id="${sub.location_id}">
+                                    <div class="name-radio-plus">
+                                        <div class="input">
+                                            <input required type="radio" name="selectLocation" value="${sub.location_id}" id="check-${sub.location_id}">
+                                            <label for="check-${sub.location_id}">${sub.name}</label>
+                                        </div>
+                                    </div>
+                                </li>`;
+                        }
+                    ul += '</ul>';
+                }
+                let $li = $(`
+                <li class="list-division" id="${loc.location_id}">
+                    <div class="name-radio-plus">
+                        <div class="input">
+                            <input required type="radio" name="selectLocation" value="${loc.location_id}" id="check-${loc.location_id}">
+                            <label for="check-${loc.location_id}">${loc.name}</label>
+                        </div>
+                    </div>
+                    ${ul}
+                </li>`);
 
                 ulModal.append($li);
                 count++;
             }
-          }
-        } else {
-          for (const loc of this.locations) {
-            if (count <= maxDisplay) {
-              const $lh = $(`<lh><h6>${loc['parent_location_name']}</h6></lh>`);
-              ulModal.append($lh);
-              if ('sublocations' in loc) {
-                for (const subloc of loc.sublocations) {
-                  const $li = $(`
-                      <li class="list-division" id="${subloc.id}">
-                          <div class="name-radio-plus">
-                              <div class="input">
-                                  <input required type="radio"
-                                  name="selectLocation"
-                                  value="${subloc.id}" id="check-${subloc.id}">
-                                  <label for="check-${subloc.id}">${subloc.name}</label>
-                              </div>
-                          </div>
-                      </li>`);
-                  ulModal.append($li);
-                }
-              }
-              count++;
-            }
-          }
         }
     }
 
@@ -304,8 +351,8 @@ export class AddMobilityImpairedComponent implements OnInit, OnDestroy {
               if ('sublocations' in loc) {
                 for (const sublocs of loc['sublocations']) {
                   if (sublocs['id'] == selectedLocationId) {
-                    this.selectedUser['location_name'] = `${loc['parent_location_name']}, ${sublocs['name']}`;
-                    if (/^[_-\s]$/.test(loc['parent_location_name'])) {
+                    this.selectedUser['location_name'] = `${loc['name']}, ${sublocs['name']}`;
+                    if (/^[_-\s]$/.test(loc['name'])) {
                       this.selectedUser['location_name'] = `${sublocs['name']}`;
                     }
                     locationFound = true;
@@ -398,67 +445,91 @@ export class AddMobilityImpairedComponent implements OnInit, OnDestroy {
     }
 
     onKeyUpSearchModalLocationEvent(){
-      this.searchModalLocationSubs = Observable.fromEvent(this.modalSearchLocation.nativeElement, 'keyup')
-      .debounceTime(500)
-      .subscribe((event) => {
-      this.formLocValid = false;
-      let value = event['target'].value,
-          result = [];
-      let seenSubLocIndex = [];
-      const seenIndex = [];
-      let findRelatedName;
+        this.searchModalLocationSubs = Observable.fromEvent(this.modalSearchLocation.nativeElement, 'keyup')
+        .debounceTime(500)
+        .subscribe((event) => {
+            this.formLocValid = false;
+            let value = event['target'].value,
+            result = [];
+            let seenSubLocIndex = [];
+            const seenIndex = [];
+            let findRelatedName;
 
-      if (parseInt(this.selectedUser['account_role_id'], 10) === 1 ||
-          parseInt(this.selectedUser['account_role_id'], 10) === 11 ||
-          parseInt(this.selectedUser['account_role_id'], 10) === 15 ||
-          parseInt(this.selectedUser['account_role_id'], 10) === 16 ||
-          parseInt(this.selectedUser['account_role_id'], 10) === 18
-      ) {
-        findRelatedName = (data, mainParent?) => {
-          for(let i in data) {
-              if(data[i]['name'].toLowerCase().indexOf(value.toLowerCase()) > -1){
-                  result.push(data[i]);
-              }
-          }
-          return result;
-        };
-      } else {
+            if (parseInt(this.selectedUser['account_role_id'], 10) === 1 ||
+                parseInt(this.selectedUser['account_role_id'], 10) === 11 ||
+                parseInt(this.selectedUser['account_role_id'], 10) === 15 ||
+                parseInt(this.selectedUser['account_role_id'], 10) === 16 ||
+                parseInt(this.selectedUser['account_role_id'], 10) === 18
+                ) {
+                findRelatedName = (data, mainParent?) => {
+                    for(let i in data) {
+                        if(data[i]['name'].toLowerCase().indexOf(value.toLowerCase()) > -1){
+                            result.push(data[i]);
+                        }
+                    }
+                    return result;
+                };
+            } else {
 
-        findRelatedName = (data, mainParent?) => {
-          for ( let i = 0; i < data.length; i++) {
-            if (data[i]['parent_location_name'].toLowerCase().indexOf(value.toLowerCase()) > -1) {
-              result.push(data[i]);
+                findRelatedName = (data, mainParent?) => {
+                    for ( let i = 0; i < data.length; i++) {
+                        if (data[i]['parent_location_name'].toLowerCase().indexOf(value.toLowerCase()) > -1) {
+                            result.push(data[i]);
+                        }
+                    }
+                    for ( let i = 0; i < data.length; i++) {
+                        seenSubLocIndex = [];
+                        for (let s = 0; s < data[i]['sublocations'].length; s++) {
+                            if (data[i]['sublocations'][s]['name'].toLowerCase().indexOf(value.toLowerCase()) > -1) {
+                                if (seenIndex.indexOf(i)) {
+                                    seenIndex.push(i);
+                                }
+                                seenSubLocIndex.push(data[i]['sublocations'][s]);
+                                data[i]['sublocations'] = seenSubLocIndex;
+                            }
+                        }
+                    }
+                    for (let si = 0; si < seenIndex.length; si++) {
+                        result.push(data[seenIndex[si]]);
+                    }
+                    return result;
+                };
             }
-          }
-          for ( let i = 0; i < data.length; i++) {
-              seenSubLocIndex = [];
-              for (let s = 0; s < data[i]['sublocations'].length; s++) {
-                if (data[i]['sublocations'][s]['name'].toLowerCase().indexOf(value.toLowerCase()) > -1) {
-                  if (seenIndex.indexOf(i)) {
-                    seenIndex.push(i);
-                  }
-                  seenSubLocIndex.push(data[i]['sublocations'][s]);
-                  data[i]['sublocations'] = seenSubLocIndex;
+
+            findRelatedName = (data, mainParent?) => {
+                for ( let i = 0; i < data.length; i++) {
+                    if (data[i]['name'].toLowerCase().indexOf(value.toLowerCase()) > -1) {
+                        result.push(data[i]);
+                    }
                 }
-              }
-            }
-            for (let si = 0; si < seenIndex.length; si++) {
-              result.push(data[seenIndex[si]]);
-            }
-          return result;
-        };
-      }
+                for ( let i = 0; i < data.length; i++) {
+                    seenSubLocIndex = [];
+                    for (let s = 0; s < data[i]['sublocations'].length; s++) {
+                        if (data[i]['sublocations'][s]['name'].toLowerCase().indexOf(value.toLowerCase()) > -1) {
+                            if (seenIndex.indexOf(i)) {
+                                seenIndex.push(i);
+                            }
+                            seenSubLocIndex.push(data[i]['sublocations'][s]);
+                            data[i]['sublocations'] = seenSubLocIndex;
+                        }
+                    }
+                }
+                for (let si = 0; si < seenIndex.length; si++) {
+                    result.push(data[seenIndex[si]]);
+                }
+                return result;
+            };
 
-      if(value.length > 0){
-          result = [];
-          findRelatedName( JSON.parse(JSON.stringify(this.locationsCopy)) );
-          this.locations = result;
-      }else{
-          this.locations = JSON.parse(JSON.stringify(this.locationsCopy));
-      }
+            if(value.length > 0){
+                result = [];
+                findRelatedName( JSON.parse(JSON.stringify(this.locationsCopy)) );
+                this.locations = result;
+            }else{
+                this.locations = JSON.parse(JSON.stringify(this.locationsCopy));
+            }
 
-        this.buildLocationsListInModal();
-      });
+            this.buildLocationsListInModal();
+        });
     }
 
     ngOnDestroy(){
