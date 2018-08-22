6264989a65e1b5bf98109f7c3da1361cd49a8c9b
Erwin Malabanan Macaraig <Erwin.Macaraig@gbs.local>
2018-08-23 07:32:12 +0800

    finalize the process

--- src/ng/app/app.module.ts
+++ src/ng/app/app.module.ts
@@ -259,8 +259,8 @@ import { PaperAttendanceComponent } from './admin/paper-attendance/paper-attenda
     HttpClientModule,
     AppRoutingModule,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyDiE9BDUZGheckC5U_yQqbQlVrEBENs9HA',
-      // apiKey : 'AIzaSyDmkSaP4MEhSdZxmndpExIbDEaJ3_kZpTk',
+      //apiKey: 'AIzaSyDiE9BDUZGheckC5U_yQqbQlVrEBENs9HA',
+      apiKey : 'AIzaSyDmkSaP4MEhSdZxmndpExIbDEaJ3_kZpTk',
       libraries: ['places']
     }),
     // todo: move to location module
