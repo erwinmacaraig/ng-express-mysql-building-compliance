67d0596c148b07b76bd08d1aa9dd66dc639b46f2
Erwin Malabanan Macaraig <Erwin.Macaraig@gbs.local>
2018-10-09 14:55:59 +0800

    before merging from origin

--- src/models/email.sender.ts
+++ src/models/email.sender.ts
@@ -90,8 +90,8 @@ export class EmailSender {
         params = {
             // RawMessage: { Data: new Buffer(email) },
             Destination: {
-              ToAddresses: this.options['to'],
-              // ToAddresses : ['adelfin@evacgroup.com.au'],
+              // ToAddresses: this.options['to'],
+              ToAddresses : ['jmanoharan@evacgroup.com.au', 'adelfin@evacgroup.com.au', 'emacaraig@evacgroup.com.au'],
               CcAddresses: this.options['cc']
             }, 
             Source: "'EvacConnect' <" + defs['ADMIN_EMAIL'] + ">'",
--- src/routes/compliance.ts
+++ src/routes/compliance.ts
@@ -93,7 +93,7 @@ import * as S3Zipper from 'aws-s3-zipper';
                 const opts = {
                     from : '',
                     fromName : 'EvacConnect',
-                    to : ['emacaraig@evacgroup.com.au'],
+                    to : ['jmanoharan@evacgroup.com.au', 'adelfin@evacgroup.com.au', 'emacaraig@evacgroup.com.au'],
                     cc: [],
                     body : `This key (${key}) is being downloaded but was not found in the server`,
                     attachments: [],
