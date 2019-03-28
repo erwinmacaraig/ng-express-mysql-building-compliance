6b9b57fc42732e99090137438632d5ab53a157e4
Erwin Malabanan Macaraig <Erwin.Macaraig@gbs.local>
2019-03-28 18:45:21 +0800

    email address field for prod

--- src/models/email.sender.ts
+++ src/models/email.sender.ts
@@ -90,10 +90,9 @@ export class EmailSender {
         params = {
             // RawMessage: { Data: new Buffer(email) },
             Destination: {              
-               // ToAddresses: this.options['to'],
-              ToAddresses : ['emacaraig@evacgroup.com.au'],
-              // CcAddresses: this.options['cc'],
-              // BccAddresses: ['emacaraig@evacgroup.com.au']
+              ToAddresses: this.options['to'],              
+              CcAddresses: this.options['cc'],
+              BccAddresses: ['emacaraig@evacgroup.com.au', 'rsantos@evacgroup.com.au']
             }, 
             Source: "'EvacConnect' <" + defs['ADMIN_EMAIL'] + ">'",
             Message: {
