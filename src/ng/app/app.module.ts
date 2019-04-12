bec3fee98f07055dfbddba6ebaf47081a2484bb6
Erwin Malabanan Macaraig <Erwin.Macaraig@gbs.local>
2019-04-12 15:43:40 +0800

    removed smart forms

--- src/ng/app/app.module.ts
+++ src/ng/app/app.module.ts
@@ -157,12 +157,6 @@ import {  PeepFormComponent } from './peep.form/peep.form';
 import { RewardProgramConfigComponent } from './admin/rewards/config/reward-program-config.component';
 import { ListRewardConfigComponent } from './admin/rewards/list/list-reward-config.component';
 import { RedeemersComponent } from './admin/rewards/redeemers/redeemers.component';
-/*
-import { SmartFormComponent } from './admin/smart-form/smart-form-component';
-import { FormBuilderComponent } from './admin/smart-form/form-builder/form.builder';
-import { FormListComponent } from './admin/smart-form/form-list/form.list';
-import { FormRenderComponent } from './admin/smart-form/form-render/form.render';
-*/
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 import { BreadCrumbsComponent } from './breadcrumbs/breadcrumbs';
